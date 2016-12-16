c819e60cea0a763db80e5861c853cb027d93c70a
takshingli810 <takshingli810@gmail.com>
2016-12-15 23:26:49 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function(){
+  console.log("loaded");
+
+  // global variable
+
+
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+    console.log(wScroll);
+
+    });
+
+});
